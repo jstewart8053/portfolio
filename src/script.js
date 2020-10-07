1101bd56b0e0a32a21c7d3c35f1cd9ec3c03a9c2
Jennifer Stewart <jstewart8053@gmail.com>
2020-10-07 14:18:58 -0500

    added main files

---
+++ src/script.js
@@ -0,0 +1,11 @@
+/*document.body.onload = function() {
+  
+  var body = document.querySelector('body');
+  var homeBtn = document.getElementsByTagName('button')[0];
+  window.addEventListener('scroll', function(event) {
+    window.scrollY > body.style.height+  300 ? homeBtn.style.visbility = 'visible':
+    homeBtn.style.visibility = 'visible';
+   
+  })
+}
+ */ 
